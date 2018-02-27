5ea1ef298d9df49e21603e9d37c69a8f730d4ae0
ZhangXuemeng <Shirley@ZhangXuemengs-MacBook.local>
2018-02-27 16:35:23 -0500

    generate random colors for squares

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,9 @@
-var colors= [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(255, 0, 255)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)"
-]
+var colors= generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay= document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,9 +16,48 @@ for (var i = 0; i < squares.length; i++) {
 		var clickedColor = this.style.background;
 		//compare color to pickedColor
 		if(clickedColor === pickedColor){
-			alert("Correct!")
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		} else{
-			alert("Wrong!")
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
+}
+
+function changeColors(color){
+	//loop through all squares
+	for (var i = 0; i < squares.length; i++){
+		//change each color to macth given color
+		squares[i].style.background = color;
+	}
+
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make an array
+	var arr=[];
+	//add num random colors to array
+	for (var i = 0; i < num; i++){
+		arr.push(randomColor());
+		//get random color and push into arr
+	}
+	//return that array
+	return arr;
+}
+
+function randomColor(){
+	//pick a red from 0-255
+	var r = Math.floor(Math.random() * 256);
+	//pick green
+	var g = Math.floor(Math.random() * 256);
+	//pick blue
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
